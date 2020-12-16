2db1b41f08fd514940af4b5e095bee5a6c0eeb8a
wuzhenzhen <995354332@qq.com>
2020-12-16 11:10:33 +0800

    ..

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@
  * @Description: Description
  * @Author: ranli
  * @Date: 2020-12-15 17:40:52
- * @LastEditTime: 2020-12-15 18:05:06
+ * @LastEditTime: 2020-12-16 11:03:42
  * @LastEditors: ranli
  */
 var firebaseConfig = {
@@ -16,68 +16,11 @@ var firebaseConfig = {
 };
 
 firebase.initializeApp(firebaseConfig);
-const defaultAnalytics = firebase.analytics()
-defaultAnalytics.logEvent('analytics-index',{
-  proId:4546545,
-  proName:"产品名称",
-  prodesc:"产品名称产品名称产品名称",
-  payment_type:"3",
+
+
+ var defaultAnalytics = firebase.analytics()
+ defaultAnalytics.setUserProperties({
+  favorite_food : 'apples',
+  lover:"zz"
 });
-defaultAnalytics.setAnalyticsCollectionEnabled(true);
-defaultAnalytics.setAnalyticsCollectionEnabled("analytics-index");
-defaultAnalytics.setUserProperties('analytics-props');
-defaultAnalytics.setUserId('analytics-id');
-firebase.auth().tenantId = 'Comments';
-
-
-function add_shipping_info() {
-  defaultAnalytics.logEvent('add_shipping_info',{
-    proId:4546545,
-    proName:"产品名称aaa",
-    prodesc:"产品名称产品名称产品名称aaa",
-    coupon:"mo",
-    currency:"156",
-    items:[],
-    payment_type:"2",
-    shipping_tier:"156",
-    tax:"422",
-    value:156,
-  });
-  console.log("add_shipping_info");
-}
-
-function add_payment_info() {
-  defaultAnalytics.logEvent('add_payment_info',{
-    proId:4546545,
-    proName:"产品名称gdfdaaa",
-    prodesc:"产品名称产品gdgefg 名称产品名称aaa",
-    coupon:"mo1741",
-    currency:"fsdfdf",
-    items:[],
-    payment_type:"3",
-    shipping:"add_shipping_info",
-    shipping_tier:"15hhghgh6",
-    value:156,
-  });
-  console.log("add_payment_info");
-}
-
-function add_to_cart() {
-  defaultAnalytics.logEvent('add_to_cart',{
-    currency:4546545,
-    items:"ggg",
-    value:156,   
-    shipping:"add_shipping_info",
-  });
-  console.log("add_to_cart");
-}
-
-function login() {
-  defaultAnalytics.logEvent('login',{
-    username:156156,
-    psd:"产品名称aaa",
-  });
-  console.log("login");
-}
-
-
+defaultAnalytics.setUserId('buy-Product-id');
